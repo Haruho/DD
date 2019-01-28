b7e4054a736487f1a19e91169cf732317b29eed0
youzi <532685083@qq.com>
2019-01-28 22:06:14 +0800

    Get item info

--- Assets/Scripts/Prepare/ItemDrag.cs
+++ Assets/Scripts/Prepare/ItemDrag.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 /// <summary>
 /// 实现UI的拖拽效果
 /// </summary>
 public class ItemDrag : MonoBehaviour{
     Transform backpack;
     Transform warehouse;
+    Text infoText;
+    //存放信息的变量
+    string[] info = new string[] { };
     private void Start()
     {
         backpack = GameObject.Find("Canvas/backpack").transform;
         warehouse = GameObject.Find("Canvas/warehouse").transform;
+        infoText = GameObject.Find("Canvas/ItemIntro/Text").GetComponent<Text>();
     }
     /// <summary>
     /// Item的点击效果，从仓库到背包
@@ -27,5 +32,20 @@ public class ItemDrag : MonoBehaviour{
         {
             transform.SetParent(backpack, false);
         }
+    }
+    public void PointerEnter()
+    {
+        if (info.Length == 0)
+        {
+            info = PrepareInterface.GetItemInfo(transform.name);
+        }
+        //给text赋值
+        infoText.text = "名称：" + info[0] + "\n"
+                        + "花费木材：" + info[1] + "\n"
+                        + "花费金属：" + info[2];
+    }
+    public void PointerExit()
+    {
+        infoText.text = "";
     }
 }
--- Assets/Scripts/Prepare/PrepareInterface.cs
+++ Assets/Scripts/Prepare/PrepareInterface.cs
@@ -10,6 +10,7 @@ public class Prepare
 public class PrepareInterface : MonoBehaviour {
     private GameObject blueprintButton;
     private Transform bpParent;
+    private static List<Blueprint> blueprint;
 	// Use this for initialization
 	void Start () {
         blueprintButton = Resources.Load("Prefabs/UIPrefabs/bluePrintButton") as GameObject;
@@ -25,11 +26,33 @@ public class PrepareInterface : MonoBehaviour {
     void CreateInterface()
     {
         //获取
-        List<Blueprint> blueprints = ReadBuleprintData.instance.GetOwnBlueprint(Prepare.filename);
-        for (int i =0;i<blueprints.Count;i++)
+        blueprint = ReadBuleprintData.instance.GetOwnBlueprint(Prepare.filename);
+        for (int i =0;i<blueprint.Count;i++)
         {
             GameObject go = Instantiate(blueprintButton);
             go.transform.SetParent(bpParent,false);
+            go.transform.name = blueprint[i].name;
         }
+    }
+    /// <summary>
+    /// 获取Item的Info
+    /// </summary>
+    /// <param name="name">Item的名称标识</param>
+    /// <returns>Item Info</returns>
+    public static string[] GetItemInfo(string name)
+    {
+        string[] info = new string[] { };
+        for (int i =0;i<blueprint.Count;i++)
+        {
+            if (blueprint[i].name == name)
+            {
+                info = new string[] { blueprint[i].name, blueprint[i].woodExpend.ToString(), blueprint[i].metalExpend.ToString() };
+            }
+            else
+            {
+                print("No Such of Item");
+            }
+        }
+        return info;
     }
 }
