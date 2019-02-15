1a0c113d6ec596af43d1e789d678a3575637604e
Haruho <532685083@qq.com>
2019-02-15 16:04:56 +0800

    res updata

--- Assets/Scripts/ResourcesInstance.cs
+++ Assets/Scripts/ResourcesInstance.cs
@@ -9,12 +9,15 @@ public class ResourcesInstance : MonoBehaviour {
     private Transform m_transform;
     private BoxCollider2D m_collider;
     private Animator anim;
+    //金属碎片的预设提
+    private GameObject metal_slice;
 	// Use this for initialization
 	void Start () {
         life = 100;
         m_transform = transform;
         m_collider = transform.GetComponent<BoxCollider2D>();
         anim = GetComponent<Animator>();
+        metal_slice = Resources.Load("ResourcesResidue/metal_slice") as GameObject;
     }
 	
 	// Update is called once per frame
@@ -47,8 +50,30 @@ public class ResourcesInstance : MonoBehaviour {
                 //取消资源实例的交互
                 m_collider.enabled = false;
                 //播放取消动画
-                anim.SetBool("gather_finish",true);
+                anim.SetBool(type.ToString() + "_gf",true);
             }
         }
+    }
+    /// <summary>
+    /// 金属收集完毕的行为
+    /// </summary>
+    public void MetalGatherFinish()
+    {
+        print("Metal Gather Finish Behaviour");
+        for (int i =0;i<3; i++)
+        {
+            GameObject go = Instantiate(metal_slice,m_transform.position,Quaternion.identity);
+            go.name = "metal_slice" + i.ToString();
+            //赋予随机的力
+            go.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-10,10),10));
+        }
+    }
+    /// <summary>
+    /// 树木收集完毕的行为
+    /// </summary>
+    public void WoodGatherFinish()
+    {
+        print("Wood Gather Finish Behaviour");
+        m_transform.GetChild(0).gameObject.SetActive(true);
     }
 }
---
+++ Assets/testforslice.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testforslice : MonoBehaviour {
+    Rigidbody2D rigi;
+	// Use this for initialization
+	void Start () {
+        rigi = transform.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("Add force");
+            rigi.AddForce(new Vector2(Random.Range(-10,10),10));
+        }
+	}
+}
